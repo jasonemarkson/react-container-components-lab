fefe010e9f5f437ebed57b508cadea349ee98898
jasonemarkson <jasonmarkson76@gmail.com>
2021-08-09 21:07:06 -0400

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,14 @@
 // Code MovieReviews Here
+import React from 'react';
+
+const MovieReviews = ({display_title, summary_short, publication_date}) => {
+    <div className="review-list">    
+        <div className="review">
+            <h2>{display_title}</h2>
+            <em>Published: {publication_date}</em>
+            <p>{summary_short}</p>
+        </div>
+    </div>
+}
+
+export default MovieReviews
