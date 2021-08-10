2c7ac308942488b6784337bafbf460b9ce1cf490
jasonemarkson <jasonmarkson76@gmail.com>
2021-08-10 00:02:38 -0400

    commit tree 594f42a9f3b3020fbd61fa1da0f319a6d08f1c25

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,13 +1,17 @@
 // Code MovieReviews Here
 import React from 'react';
 
-const MovieReviews = ({display_title, summary_short, publication_date}) => {
-    <div className="review-list">    
-        <div className="review">
-            <h2>{display_title}</h2>
-            <em>Published: {publication_date}</em>
-            <p>{summary_short}</p>
-        </div>
+const MovieReviews = (reviews) => {
+    return <div className="review-list"> 
+        { reviews.reviews.map(r => <MovieReview display_title={r.display_title} summary_short={r.summary_short} publication_date={r.publication_date} />) }
+    </div>
+}
+
+const MovieReview = ({display_title, summary_short, publication_date}) => {
+    return <div className="review">
+        <h1>{display_title}</h1>
+        <em>Published: {publication_date}</em>
+        <p>{summary_short}</p>
     </div>
 }
 
--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -2,8 +2,37 @@ import React, { Component } from 'react';
 import 'isomorphic-fetch';
 import MovieReviews from './MovieReviews'
 
-const NYT_API_KEY = 'your-key-here';
+const NYT_API_KEY = '9tNAuf9byusVRKd4dIOsam7pco4Goa5w';
 const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code SearchableMovieReviewsContainer Here
+class SearchableMovieReviewsContainer extends Component {
+
+    state = {
+        reviews: [],
+        searchTerm: ""
+    }
+
+    componentDidMount() {
+        fetch(URL)
+        .then(response => response.json())
+        .then(movieReviews => 
+            
+            this.setState ({
+                reviews: movieReviews.results
+            })
+        
+        )
+    }
+
+    render() {
+        return (
+            <div className="searchable-movie-reviews">
+                <MovieReviews reviews={this.state.reviews} />
+            </div>
+        )
+    }
+}
+
+export default SearchableMovieReviewsContainer;
